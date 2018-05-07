801104c6ebddfbd40903e9ca7a181f9a4609550a
florina <fanderl95@gmail.com>
2018-05-07 13:14:18 +0200

    Update

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/* Source File: Computational Physics 2018
+ * main.cpp
+ *
+ *  Created on: 07.05.2018
+ *      Author: Sara Gasparini
+ */
+
+#include "liesegang.h"
+#include "Defines.h"
+
+
+/*args = [
+ * -
+ * -
+ * -
+ *
+ * ]*/
+int main(int args, char * argv[])
+{
+
+
+
+	return 0;
+}
+
+
