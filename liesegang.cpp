e9862c34294a3ddff96abff481fa9c18ea9f8934
florina <fanderl95@gmail.com>
2018-05-07 12:00:58 +0200

    Initial Commit

---
+++ liesegang.cpp
@@ -0,0 +1,18 @@
+/* Source File: Computational Physics 2018
+ * Liesegang.cpp
+ *
+ *  Created on: 07.05.2018
+ *      Author: Sara Gasparini
+ */
+
+#include "Liesegang.h"
+
+Liesegang::Liesegang() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Liesegang::~Liesegang() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ liesegang.h
@@ -0,0 +1,24 @@
+/* Source File: Computational Physics 2018
+ * Liesegang.h
+ *
+ *  Created on: 07.05.2018
+ *      Author: Sara Gasparini
+ */
+
+#ifndef LIESEGANG_H_
+#define LIESEGANG_H_
+
+
+/*Class for Simulation of Liesegang phenomenon*/
+class Liesegang {
+public:
+	Liesegang();
+	virtual ~Liesegang();
+
+
+
+private:
+
+};
+
+#endif /* LIESEGANG_H_ */
